9e13f4aa6a8a9cf5b6e15aad2966712696013c70
Pre0 <drayn8@gmail.com>
2024-10-15 14:59:43 -0400

    added delete

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,9 @@ let books = [];
             <p><strong>Book Name: </strong>${book.name}</p>
             <p><strong>Author Name:</strong>${book.authorName}<p>
             <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-            <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+            <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+            <button onclick="editbook(${index})">Edit</button>
+            <button onclick="deletebook(${index})">Delete</button>`
         );
         document.getElementById('books').innerHTML = booksDiv.join('');
     }
@@ -34,4 +36,8 @@ let books = [];
         document.getElementById('authorName').value = '';
         document.getElementById('bookDescription').value = '';
         document.getElementById('pagesNumber').value = '';
+    }
+    function deletebook() {
+        books.splice(book => index, 1);
+        showbooks();
     }
