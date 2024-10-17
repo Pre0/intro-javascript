91fbe2557c291f742598b4e64b48315a087a0cf3
Pre0 <drayn8@gmail.com>
2024-10-17 14:23:09 -0400

    fixed line

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -21,14 +21,14 @@ function generateCards(){
         gameContainer.appendChild(card);
     }
 }
-
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i]], array[j]] = [array[j], array[i]];
+        [array [i], array[j]] = [array[j], array[i]];
     }
     return array;
 }
+
 
 function handleCardClick(event) {
     const card = event.target;
